61cb5e3c365cb2b653b1693725068f22aad59a59
Vokires94 <36535033+Vokires94@users.noreply.github.com>
2019-01-30 12:37:24 +0200

    Ready beta version.

--- Buttons.js
+++ Buttons.js
@@ -20,6 +20,8 @@ let level = 1;
 
 let difficult = 0;
 
+let next_level = 0;
+
 function shadow_buttons() {
     for (let z = 0; z < 4; z++) {
                 document.getElementsByClassName(arr[z])[0].disabled=true;
@@ -72,7 +74,8 @@ class Buttons extends React.Component {
         this.handleStartBlink = this.handleStartBlink.bind(this);
     }
 
-    handleClick(item) {
+
+        handleClick(item) {
 
 
         paint_buttons();
@@ -85,24 +88,28 @@ class Buttons extends React.Component {
 
         tries++;
 
-        if (tries === 5) {
-            for (let i = 0; i <= 4; i++) {
+        if (tries === 5+next_level) {
+            for (let i = 0; i <= 4+next_level; i++) {
                 if (res_user[i] === user_mas[i]) points++;
                 else {
                     document.getElementById ('message_user').textContent = 'Sorry, you wrong. You have completed ' + leg + ' leg';
-                    lose = true;
+                    lose = true; next_level=0;
                     break;
                 }
             }
-            if(points === 5) leg++;
+            if(points === 5+next_level) leg++;
 
             if(!lose)document.getElementById ('message_user').textContent = 'You have completed ' + leg + ' leg';
             tries = 0;
             points= 0;
-            //document.getElementById ('go_play').disabled = false;
+
             paint_buttons();
-            shadow_buttons();shadow_buttons();
-            lose!==true ? this.handleStartBlink() : document.getElementById ('go_play').disabled = false;
+            shadow_buttons();
+            if(lose!==true) { next_level++; this.handleStartBlink(); }
+            else {
+                document.getElementById ('change_level').disabled = false;
+                document.getElementById ('go_play').disabled = false;
+            }
         }
 
     }
@@ -116,11 +123,27 @@ class Buttons extends React.Component {
          }
     }
 
-    handleStartBlink() {
-
+    handleReset(){
         res_user = [];
         user_mas = [];
+        tries = 0;
+        points = 0;
+        leg = 0;
+        lose = false;
+        level = 1;
+        next_level = 0;
+        paint_buttons();
+        shadow_buttons();
+        document.getElementById ('go_play').disabled = false;
+        document.getElementById ('message_user').textContent = "Let's play.";
+    }
 
+    handleStartBlink() {
+        console.log(next_level);
+        res_user = [];
+        user_mas = [];
+        document.getElementById ('change_level').disabled = true;
+        document.getElementById ('reset_button').disabled = true;
        document.getElementById ('go_play').disabled = true;
 
         //console.log(Math.round(Math.random() * (4 - 1) + 1));
@@ -128,13 +151,16 @@ class Buttons extends React.Component {
         var timer = setInterval(Rand_blink, 3000-difficult);
 
         setTimeout(function() {
-            clearInterval(timer)}, 15000-difficult*5);
+            clearInterval(timer)}, 15000+next_level*3000-difficult*(5+next_level));
 
         setTimeout(() => {
             for (var i = 0; i < 4; i++) {
                 document.getElementsByClassName(buttons[i].id)[0].disabled = false;
             }
-        }, 17000-difficult*5);
+            document.getElementById ('reset_button').disabled = false;
+            document.getElementById ('message_user').textContent = 'Tip chosen buttons';
+        }, 17000+next_level*3000-difficult*(5+next_level));
+
 
 
         function Rand_blink() {
@@ -144,13 +170,13 @@ class Buttons extends React.Component {
 
             console.log(arr[rand]);
 
-            for (var i = 0; i < 4; i++) {
-                document.getElementsByClassName(arr[i])[0].style.background = arr2[i];
-            }
+            paint_buttons();
 
             var current = document.getElementsByClassName(arr[rand])[0].style.background;
 
             document.getElementsByClassName(arr[rand])[0].style.background = 'pink';
+
+
 
             setTimeout(function() {
                 document.getElementsByClassName(arr[rand])[0].style.background = current;
@@ -187,6 +213,7 @@ class Buttons extends React.Component {
                 <br />
                 <button id='go_play' onClick={this.handleStartBlink}>Start</button>
                 <button id='change_level' onClick={this.handleLevelChange}>Easy</button>
+                <button id='reset_button' onClick={this.handleReset}>Reset</button>
             </div>
         );
     }
